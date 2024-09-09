cb71286b099aada413e4c38d11b49c41fd41f3ed
LocalRepo <riteshshende616@gmail.com>
2024-09-09 20:45:24 +0530

    Integrated API and POST or FETCH data in mysql using Node js EXPRESS and postman

--- app.js
+++ app.js
@@ -1,11 +1,56 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var mysql  = require("mysql");
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+  extended:true,
+})
+);
 
-
-app.get('/', (req, res) => {
-  res.send("Hi this is Ritesh Shende");
+const con = mysql.createConnection({
+  host: 'localhost',
+  user:"root",
+  password:"",
+  database:"electricity_management"
+});
+con.connect((err) =>{
+  if(err)
+  {
+    console.log(err)
+  }else{
+    console.log("connected")
+  }
 });
 
-app.listen(3000, () => {
-  console.log("listing the port 3000");
-});
+
+
+// app.get("/",(req,res)=>{
+// res.json({message: "ok" });
+// });
+
+
+app.post('/post', (req,res)=>{
+    const name=req.body.name;
+    const email=req.body.email;
+    const phone_no=req.body.phone_no;
+    const password=req.body.password;
+    const confirm_password=req.body.confirm_password;
+    const address=req.body.address;
+    con.query('insert into user values (?,?,?,?,?,?)',[name,email,phone_no,password,confirm_password,address],(err,result)=>{
+        if(err)
+            {
+              console.log(err)
+            }else{
+              res.send("POSTED")
+            }
+    })
+})
+
+app.listen(3000,(err)=>{
+    if(err)
+        {
+          console.log(err)
+        }else{
+          console.log("on the port 3000")
+        }
+})
