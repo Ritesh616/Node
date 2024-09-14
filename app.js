f8010e0f2d0bbfd0d2c69f34bc566b3139424896
Aniket150293 <aniketbansode15@gmail.com>
2024-09-14 18:48:06 +0530

    First Commit

--- app.js
+++ app.js
@@ -1,56 +1,56 @@
-var express = require('express');
-var mysql  = require("mysql");
+var express = require("express");
+var mysql = require("mysql");
 var app = express();
 app.use(express.json());
-app.use(express.urlencoded({
-  extended:true,
-})
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
 );
 
-const con = mysql.createConnection({
-  host: 'localhost',
-  user:"root",
-  password:"",
-  database:"electricity_management"
+// const con = mysql.createConnection({
+//   host: 'localhost',
+//   user:"root",
+//   password:"",
+//   database:"electricity_management"
+// });
+// con.connect((err) =>{
+//   if(err)
+//   {
+//     console.log(err)
+//   }else{
+//     console.log("connected")
+//   }
+// });
+
+app.get("/", (req, res) => {
+  res.json({ message: "ok" });
 });
-con.connect((err) =>{
-  if(err)
-  {
-    console.log(err)
-  }else{
-    console.log("connected")
+
+app.post("/user", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const phone_no = req.body.phone_no;
+  const password = req.body.password;
+  const confirm_password = req.body.confirm_password;
+  const address = req.body.address;
+  con.query(
+    "insert into user values (?,?,?,?,?,?)",
+    [name, email, phone_no, password, confirm_password, address],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("POSTED");
+      }
+    }
+  );
+});
+
+app.listen(3000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("on the port 3000");
   }
 });
-
-
-
- app.get("/",(req,res)=>{
- res.json({message: "ok" });
- });
-
-
-app.post('/post', (req,res)=>{
-    const name=req.body.name;
-    const email=req.body.email;
-    const phone_no=req.body.phone_no;
-    const password=req.body.password;
-    const confirm_password=req.body.confirm_password;
-    const address=req.body.address;
-    con.query('insert into user values (?,?,?,?,?,?)',[name,email,phone_no,password,confirm_password,address],(err,result)=>{
-        if(err)
-            {
-              console.log(err)
-            }else{
-              res.send("POSTED")
-            }
-    })
-})
-
-app.listen(3000,(err)=>{
-    if(err)
-        {
-          console.log(err)
-        }else{
-          console.log("on the port 3000")
-        }
-})
